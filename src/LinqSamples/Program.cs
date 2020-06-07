e414c363861f2dbee7ffaf9f0928a7484858c4fc
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-07 10:08:27 +0100

    Order files in path by size desc w/o LINQ

--- src/LinqSamples/Program.cs
+++ src/LinqSamples/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
 
 namespace LinqSamples
 {
@@ -6,7 +10,49 @@ namespace LinqSamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "/Users/pedrolopes/Downloads";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine(new String('*', 50));
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            Console.WriteLine("LINQ SQL like query");
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            foreach (var file in query.Take(5))
+                Console.WriteLine($"{file.Name,-35} : {file.Length:N0}");
+
+            Console.WriteLine(new String('*', 50));
+            Console.WriteLine("LINQ method chain query");
+            var query2 = new DirectoryInfo(path).GetFiles().OrderByDescending(f => f.Length).Take(5);
+            foreach (var file in query2)
+                Console.WriteLine($"{file.Name,-35} : {file.Length:N0}");
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            Console.WriteLine("Standard way without LINQ");
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{files[i].Name,-35} : {files[i].Length:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
