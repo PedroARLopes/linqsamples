d8659155f798a891e05c2f88445b2ba9bc75459d
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-07 16:34:01 +0100

    Extend Car with more LINQ examples

--- src/CarsProj/Program.cs
+++ src/CarsProj/Program.cs
@@ -14,11 +14,22 @@ namespace CarsProj
 
             var query2 =
                 from car in cars
+                where car.Manufacturer == "BMW" && car.Year == 2016
                 orderby car.Combined descending, car.Name ascending
                 select car;
 
-            foreach (var car in query2.Take(10))
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+            // Iterate throught he sequence inside the sequence
+            var result = cars
+            .SelectMany(c => c.Name)
+            .OrderBy(c => c);
+
+            foreach (var name in result)
+            {
+                Console.WriteLine(name);
+            }
+
+            // foreach (var car in query2.Take(10))
+            //     Console.WriteLine($"{car.Name} : {car.Combined}");
         }
 
         private static List<Car> ProcessFile(string path)
@@ -27,16 +38,8 @@ namespace CarsProj
                 File.ReadAllLines(path)
                 .Skip(1)
                 .Where(l => l.Length > 0)
-                .Select(Car.ParseFromCsv)
+                .ToCar()
                 .ToList();
-        }
-
-        private static List<Car> ProcessFileQuerySyntax(string path)
-        {
-            var query = from line in File.ReadAllLines(path).Skip(1)
-                        where line.Length > 1
-                        select Car.ParseFromCsv(line);
-            return query.ToList();
         }
     }
 }
