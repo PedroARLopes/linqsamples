03a5081ff668d4efac9895c0d1e0487cc1ce0fb8
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-08 18:44:10 +0100

    Create from CSV loaded data with LINQ's help

--- src/CarsProj/Program.cs
+++ src/CarsProj/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace CarsProj
 {
@@ -9,43 +10,22 @@ namespace CarsProj
     {
         static void Main(string[] args)
         {
-            var cars = ProcessCars("fuel.csv");
-            var manufacturers = ProcessManufacturer("manufacturers.csv");
+            CreateXML();
+        }
 
-            var query =
-                from car in cars
-                group car by car.Manufacturer into carGroup
-                select new
-                {
-                    Name = carGroup.Key,
-                    Max = carGroup.Max(c => c.Combined),
-                    Min = carGroup.Min(c => c.Combined),
-                    Avg = carGroup.Average(c => c.Combined)
-                } into result
-                orderby result.Avg descending
-                select result;
+        private static void CreateXML()
+        {
+            var records = ProcessCars("fuel.csv");
 
-            var query2 =
-                cars.GroupBy(c => c.Manufacturer)
-                .Select(g =>
-                {
-                    var result = g.Aggregate(
-                        new CarStatistics(),
-                        (acc, c) => acc.Accumulate(c),
-                        acc => acc.Compute());
-                    return new
-                    {
-                        Name = g.Key,
-                        Avg = result.Average,
-                        Min = result.Min,
-                        Max = result.Max
-                    };
-                }).OrderByDescending(r => r.Avg);
+            var document = new XDocument();
+            var cars = new XElement("Cars", records.Select(record => new XElement("Car",
+                   new XAttribute("Name", record.Name),
+                   new XAttribute("Combined", record.Combined),
+                   new XAttribute("Manufacturer", record.Manufacturer)
+               )));
 
-            foreach (var result in query2)
-            {
-                Console.WriteLine($"Car: {result.Name,-30} Max: {result.Max} | Min: {result.Min} | Avg: {result.Avg:N1}");
-            }
+            document.Add(cars);
+            document.Save("fuel.xml");
         }
 
         private static List<Car> ProcessCars(string path)
